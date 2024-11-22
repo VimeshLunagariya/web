0defbf8a69442fbaa08c4128485a62adc34a3fda
Vimesh Lunagariya <vimeshlunagariya7@gmail.com>
2024-11-22 19:53:40 +0530

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3478931658"
+    serviceWorkerVersion: "2219035717"
   }
 });
